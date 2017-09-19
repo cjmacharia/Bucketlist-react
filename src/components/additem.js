df3bbb7314625567dc7096035902940379fa2be3
cjmash <collomash4@gmail.com>
2017-09-19 16:51:13 +0300

    [Create] add items functionality

--- src/components/additem.js
+++ src/components/additem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Loggedin from './nav-log.js';
 import { Redirect } from 'react-router-dom';
 import {Toaster}from 'react-toaster-js'
-import  { FormGroup,FormControl, Button }from 'react-bootstrap';
+import  { FormGroup,FormControl, Button, InputGroup,Col }from 'react-bootstrap';
 import axios from 'axios'
 class AddItem extends Component{
     constructor(props){
@@ -18,7 +18,6 @@ handleClick(event) {
     "name":this.state.name,
     }
     console.log(payload)
-    console.log(this.props.match.params)
     axios({
         url : apiUrl+'api/bucketlists/'+this.props.match.params.bucketlist_id+'/items/',
         data: payload,
@@ -28,14 +27,14 @@ handleClick(event) {
             'content_type':"application/json"
         }
     })
-    .then((response)=> {
-
+    .then(response=> {
+        alert(response.data.message)
        this.setState({redirect:true})
     }
 )
-.catch((error)=> {
+.catch(error=> {
     console.log(JSON.stringify(error));
-    alert(error.response.data.message)
+    alert(error.response.data.error)
 })
 }
 
@@ -43,15 +42,20 @@ handleClick(event) {
     {
         const redirect = this.state.redirect
         if (redirect) {
-            return <Redirect to={{pathname: '/mybuckets'}} />
+            return <Redirect to={{pathname: '/api/bucketlist/mybuckets'}} />
         }
         return(
             <div>
                 <Loggedin/>
+                <Col md = {4} mdPush={4}>
+                <h4>Add an item to this bucketlist </h4>
                 <FormGroup>
+                <InputGroup>
                 <FormControl type="text" placeholder="item name" onChange={(event)=>this.setState({name:event.target.value})} required/>
+                <InputGroup.Button><Button bsStyle="primary" onClick={(event=>this.handleClick(event))}>Submit</Button></InputGroup.Button>
+                </InputGroup>
                 </FormGroup>
-                <Button bsStyle="primary" onClick={(event=>this.handleClick(event))}>Submit</Button>
+                </Col>
                 </div>
         )
     }
