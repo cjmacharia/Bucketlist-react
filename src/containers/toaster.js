fc132d1e4ad53227859c36d9290190c526b16d48
cjmash <collomash4@gmail.com>
2017-09-19 16:59:04 +0300

    [Feature] create a toaster to handle response messages

---
+++ src/containers/toaster.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import 'react-toastify/dist/ReactToastify.min.css';
+import {ToastContainer, toast} from 'react-toastify';
+class Toaster extends Component{
+render(){
+return(
+    <ToastContainer
+    position="top-right"
+    type="default"
+    autoClose={5000}
+    hideProgressBar={true}
+    closeOnClick
+    />
+)
+}
+}
+export default Toaster
