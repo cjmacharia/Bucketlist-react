d42fde480ec93cb768cb27d70d169c616fd47f1c
cjmash <collomash4@gmail.com>
2017-09-19 16:58:00 +0300

    [Feature] create a landing page after login

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import Loggedin from './nav-log.js';
 import { Redirect } from 'react-router-dom';
-import {Toaster}from 'react-toaster-js'
-import  { FormGroup,FormControl, Button }from 'react-bootstrap';
-import axios from 'axios'
+import { toast} from 'react-toastify';
+import  { FormGroup,FormControl, Button, Col,InputGroup }from 'react-bootstrap';
+import axios from 'axios';
+import Toaster from '../containers/toaster.js';
 class Dashboard extends Component{
     constructor(props){
     super(props);
@@ -34,8 +35,9 @@ handleClick(event) {
     }
 )
 .catch((error)=> {
-    console.log(JSON.stringify(error));
-    alert(error.response.data.message)
+    if (error.response) {
+        toast.error(error.response.data.error);
+            }
 })
 }
 
@@ -43,15 +45,23 @@ handleClick(event) {
     {
         const redirect = this.state.redirect
         if (redirect) {
-            return <Redirect to={{pathname: '/mybuckets'}} />
+            return <Redirect to={{pathname: '/api/bucketlist/mybuckets'}} />
         }
         return(
             <div>
                 <Loggedin/>
+                <div className="container">
+                <Col md = {4} mdPush={4}>
+                    <Toaster/>
+                <h4>Add a bucket Name</h4>
                 <FormGroup>
+                <InputGroup>
                 <FormControl type="text" placeholder="bucketlist name" onChange={(event)=>this.setState({name:event.target.value})} required/>
+                <InputGroup.Button><Button bsStyle="primary" onClick={(event=>this.handleClick(event))}>Submit</Button></InputGroup.Button>
+                </InputGroup>
                 </FormGroup>
-                <Button bsStyle="primary" onClick={(event=>this.handleClick(event))}>Submit</Button>
+                </Col>
+                </div>
                 </div>
         )
     }
