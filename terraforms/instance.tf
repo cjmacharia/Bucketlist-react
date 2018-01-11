0ac689ee026602e979f9e5c54e6487314aff0432
cjmash <collomash4@gmail.com>
2018-01-11 10:56:27 +0300

    [add] terraform file for continous integration

---
+++ terraforms/instance.tf
@@ -0,0 +1,54 @@
+
+resource "google_compute_instance" "nat" {
+    project = "advanced-191310"
+    zone           = "europe-west3-b"
+    name           ="terra-nat-gateway"
+    can_ip_forward = true
+    machine_type   = "f1-micro"
+    tags           =  ["allow-access"]
+
+    boot_disk {
+        initialize_params {
+            image = "ubuntu-1604-xenial-v20170328"
+        }
+    }
+    network_interface {
+        subnetwork = "${google_compute_subnetwork.public-subnet.name}"
+    access_config {
+          // Ephemeral IP
+    }
+}
+}
+resource "google_compute_instance" "python" {
+    project = "advanced-191310"
+    zone = "europe-west3-b"
+    name ="terra-python-instance"
+    can_ip_forward = false
+    machine_type = "f1-micro"
+    boot_disk {
+        initialize_params {
+            image = "ubuntu-1604-xenial-v20170328"
+        }
+    }
+    network_interface {
+        subnetwork = "${google_compute_subnetwork.private-subnet.name}"
+    }
+
+
+}
+resource "google_compute_instance" "db" {
+    project = "advanced-191310"
+    zone = "europe-west3-b"
+    name ="terra-db-instance"
+    can_ip_forward = false
+    machine_type = "f1-micro"
+    boot_disk {
+        initialize_params {
+            image = "ubuntu-1604-xenial-v20170328"
+        }
+    }
+    network_interface {
+        subnetwork = "${google_compute_subnetwork.database-subnet.name}"
+
+    }
+}
