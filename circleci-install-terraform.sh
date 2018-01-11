8a724dd51b9e5ce3445f880ea4f8273e0c50568b
Collins Macharia <collomash4@gmail.com>
2018-01-11 12:44:45 +0300

    Update circleci-install-terraform.sh

--- circleci-install-terraform.sh
+++ circleci-install-terraform.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -d terraform/terraform ]; then
+if [ -d terraform/terraform ]; then
   sudo rm -rf terraform
 fi
 wget https://releases.hashicorp.com/terraform/0.6.8/terraform_0.6.8_linux_amd64.zip
