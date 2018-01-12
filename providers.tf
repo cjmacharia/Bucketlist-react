4cf773af6f2f873f54cfee33b33d554ae1d145ca
Collins Macharia <collomash4@gmail.com>
2018-01-12 11:17:39 +0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,6 @@
 // variable "region"{}
 // variable "project"{}
 provider "google" {
-    credentials = "$file("${HOME}/gcloud-service-key.json")"
     region = "europe-west3"
     project = "advanced-191310"
 }
