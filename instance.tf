1b5cce52c44ed840dc5e34451f08c33c8764e659
cjmash <collomash4@gmail.com>
2018-01-15 13:07:07 +0300

    [fix]variables

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "nat" {
     project = "advanced-191310"
     zone           = "europe-west3-b"
-    name           = "react-${random_id}"
+    name           = "react-${var.random_id}"
     can_ip_forward = true
     count = 1
     machine_type   = "f1-micro"
@@ -47,7 +47,7 @@ resource "google_compute_instance" "python" {
 
     project = "advanced-191310"
     zone = "europe-west3-b"
-    name = "python-instance-${random_id}"
+    name = "python-instance-${var.random_id}"
     can_ip_forward = false
     machine_type = "f1-micro"
     boot_disk {
@@ -66,7 +66,7 @@ resource "google_compute_instance" "python" {
 resource "google_compute_instance" "db" {
     project = "advanced-191310"
     zone = "europe-west3-b"
-    name ="db-instance-${random_id}"
+    name ="db-instance-${var.random_id}"
     can_ip_forward = false
     count = 1
     machine_type = "f1-micro"
--- network.tf
+++ network.tf
@@ -1,10 +1,10 @@
 resource "google_compute_network" "my-network" {
-    name = "my-network-${random_id}"
+    name = "my-network-${var.random_id}"
     description = "A network meant to be used by the new instance created"
     auto_create_subnetworks = "false"
 }
 resource "google_compute_firewall" "my-network-firewall" {
-  name    = "my-network-firewall-${random_id}"
+  name    = "my-network-firewall-${var.random_id}"
   network = "${google_compute_network.my-network.name}"
 
   allow {
@@ -19,7 +19,7 @@ resource "google_compute_firewall" "my-network-firewall" {
 }
 
 resource "google_compute_subnetwork" "public-subnet" {
-  name          = "public-subnet-${random_id}"
+  name          = "public-subnet-${var.random_id}"
   count         = 1
   ip_cidr_range = "10.0.1.0/24"
   network       = "${google_compute_network.my-network.name}"
@@ -36,13 +36,13 @@ resource "google_compute_firewall" "pulic" {
   }
 }
 resource "google_compute_subnetwork" "private-subnet" {
-  name          = "private-subnet-${random_id}"
+  name          = "private-subnet-${var.random_id}"
   ip_cidr_range = "10.0.2.0/24"
   network       = "${google_compute_network.my-network.name}"
   region        = "europe-west3"
 }
 resource "google_compute_subnetwork" "database-subnet" {
-  name          = "databse-subnet-${random_id}"
+  name          = "databse-subnet-${var.random_id}"
   ip_cidr_range = "10.0.3.0/24"
   network       = "${google_compute_network.my-network.name}"
   region        = "europe-west3"
