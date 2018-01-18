094d5a31577943bd59cca341345b47e4382ada05
cjmash <collomash4@gmail.com>
2018-01-18 17:16:01 +0300

    fix deployment issues

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!bin/bash
 
 set -e
+environment_variables(){
+    PROJECT_NAME: packer
+    CLOUDSDK_COMPUTE_ZONE: europe-west3-b
+    PROJECT_ID: packer-192412
+}
 clone_repo(){
     echo "clone our repo "
     git clone https://github.com/cjmash/Bucketlist-react.git
