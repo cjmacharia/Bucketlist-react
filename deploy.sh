b9b13d021def6d983dc323bf6b93523490e1dc4c
cjmash <collomash4@gmail.com>
2018-01-18 09:20:37 +0300

    update configurations

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!bin/bash
 
 set -e
-
 clone_repo(){
+    echo "clone our repo "
     git clone https://github.com/cjmash/Bucketlist-react.git
+}
+
+set_up_terraform_infrastructure(){
+    echo "create a"
+    terraform init
+    terraform apply auto-approve
 }
