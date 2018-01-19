d9a4f1e66b0f9917c9acf3ff0d2cd49b816a9587
Collins Macharia <collomash4@gmail.com>
2018-01-19 09:50:39 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ clone_repo(){
 }
 create_the_packer_image(){
     echo "creating a packer image"
-    PROJECT_ID="$PROJECT_ID" GOOGLE_CREDENTIALS="$GOOGLE_CREDENTIALS" packer packer/build.json
+    PROJECT_ID="$PROJECT_ID" GOOGLE_CREDENTIALS="$GOOGLE_CREDENTIALS" packer build packer/build.json
 }
 set_up_terraform_infrastructure(){
     echo "create the gcp infrastructure"
