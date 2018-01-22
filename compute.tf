83f5202c832e8bbd6d472bff02703e18e1a41084
cjmash <collomash4@gmail.com>
2018-01-22 17:51:53 +0300

    [update] terrraform scripts

---
+++ compute.tf
@@ -0,0 +1,27 @@
+resource "google_compute_instance_template" "instance_template" {
+  name_prefix  = "react-instance-template"
+  machine_type = "${var.machine_type}"
+  region       = "${var.region}"
+
+  disk {
+    source_image = "${var.created_image}"
+  }
+
+  network_interface {
+        subnetwork = "${google_compute_subnetwork.public-subnet.name}"
+    access_config {
+          // Ephemeral IP
+    }
+}
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_compute_instance_group_manager" "instance_group_manager" {
+  name               = "instance-group-manager"
+  instance_template  = "${google_compute_instance_template.instance_template.self_link}"
+  base_instance_name = "instance-group-manager"
+  zone               = "${var.zone}"
+  target_size        = "1"
+}
---
+++ var.tf
@@ -0,0 +1,24 @@
+variable "region"{
+    type = "string"
+    default = "europe-west1"
+}
+variable "zone" {
+  type = "string"
+  default = "europe-west3-b"
+}
+variable "machine_type" {
+  type = "string"
+  default = "n1-standard-1"
+}
+variable "max_instances" {
+  type = "string"
+  default = "4"
+}
+
+variable "min_instances" {
+  type = "string"
+  default = "2"
+}
+variable "created_image"{
+    type = "string"
+}
